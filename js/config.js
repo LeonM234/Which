de33b6345de0331d4144183151e1a6261b989ab9
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2015-01-31 17:08:56 -0600

    index.html using ng-view

--- js/config.js
+++ js/config.js
@@ -5,27 +5,27 @@
     .config(function($routeProvider){
       $routeProvider
       .when('/', {
-        templateUrl: 'views/_which_feed',
+        templateUrl: '../views/_which_feed.html',
         controller: 'feedController',
         controllerAs: 'myFeed'
       })
       .when('/login', {
-        templateUrl: 'views/_login',
+        templateUrl: '../views/_login.html',
         controller: 'loginController',
         controllerAs: 'login'
       })
       .when('/dashboard', {
-        templateUrl: 'views/_dashboard',
+        templateUrl: '../views/_dashboard.html',
         controller: 'dashboardController',
         controllerAs: 'dashboard'
       })
       .when('/about', {
-        templateUrl: 'views/_about',
-        controller: 'aboutController'.
+        templateUrl: '../views/_about.html',
+        controller: 'aboutController',
         controllerAs: 'about'
       })
       .when('/newwhich', {
-        templateUrl: 'views/_new_which',
+        templateUrl: '../views/_new_which.html',
         controller: 'newWhichController',
         controllerAs: 'newWhich'
       })
