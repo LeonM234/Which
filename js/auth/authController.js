9c5c88a1b0730cbae7d9b240d4d9cae23cc1896f
BlaiseGratton <blaisegratton@gmail.com>
2015-01-31 14:21:28 -0600

    added ref to Firebase for authentication

---
+++ js/auth/authController.js
@@ -0,0 +1,16 @@
+;(function(){
+  'use strict';
+
+  angular.module('whichApp')
+    .controller('AuthController', function($scope, $location, authFactory){
+      var vm = this;
+      vm.ref = new Firebase(FIREBASE_URL);
+      vm.ref.authWithOAuthPopup("facebook", function(error, authData) {
+        if(error){
+          console.log("login failed", error);
+        } else {
+          console.log("authenticated successfully with payload: ", authData);
+        }
+      });
+    })
+}());
